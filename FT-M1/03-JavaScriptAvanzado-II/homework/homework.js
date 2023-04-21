0bd95b22dcd1aa6a5f3eb684af42293687e0b9cc
Candela Yarossi <candyarossi@gmail.com>
2023-04-20 22:27:06 -0300

    HW M1 03

--- FT-M1/03-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1/03-JavaScriptAvanzado-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Closures
 
@@ -6,14 +6,48 @@
 La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando 
 un valor numérico que empieza en 1 e incrementa con cada invocación.
 EJEMPLO
-const nuevoContador = counter()
+const nuevoContador = counter() // 0
 nuevoContador()     // 1
 nuevoContador()     // 2
 
 const otroContador = counter()
 otroContador()      // 1
 otroContador()      // 2 */
-function counter() {}
+
+// Opción 1
+
+function counter() {
+  let num = 0;
+  return function () {
+    return ++num;
+  };
+}
+
+// Opción 2
+
+function counter() {
+  let num = 0;
+  return function () {
+    num++;
+    return num;
+  };
+}
+
+// Opción 3
+
+function counter() {
+  let num = 1;
+  return function () {
+    return num++;
+  };
+}
+
+// Opción 4
+
+function counter() {
+  let num = 0;
+  return () => ++num;
+}
 
 /* Ejercicio 2
 Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
@@ -33,24 +67,62 @@ otra vez cálculos que ya se hicieron anteriormente.
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
 
-function cacheFunction(cb) {}
+// Opción 1
+
+function cacheFunction(cb) {
+  var obj = {};
+  return function (arg) {
+    if (obj.hasOwnProperty(arg)) return obj[arg];
+    else {
+      obj[arg] = cb(arg);
+      return obj[arg];
+    }
+  };
+}
+
+// Opción 2
+
+function cacheFunction(cb) {
+  var obj = {};
+  return function (arg) {
+    if (!obj.hasOwnProperty(arg)) obj[arg] = cb(arg);
+    return obj[arg];
+  };
+}
+
+// Opción 3
+
+function cacheFunction(cb) {
+  var obj = {};
+  return (arg) => (!obj.hasOwnProperty(arg) ? (obj[arg] = cb(arg)) : obj[arg]);
+}
+
+function square(n) {
+  return n * n;
+}
+
+const squareCache = cacheFunction(square);
+squareCache(5);
+squareCache(5);
+squareCache(6);
 
 //----------------------------------------
 
 // Bind
 
 var instructor = {
-   nombre: 'Franco',
-   edad: 25,
+  nombre: "Franco",
+  edad: 25,
 };
 
 var alumno = {
-   nombre: 'Juan',
-   curso: 'FullStack',
+  nombre: "Juan",
+  curso: "FullStack",
 };
 
 function getNombre() {
-  return this.nombre;}
+  return this.nombre;
+}
 
 /*
   Ejercicio 3
@@ -58,8 +130,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor = getNombre.bind();
-let getNombreAlumno = getNombre.bind();
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
@@ -67,22 +139,22 @@ let getNombreAlumno = getNombre.bind();
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
-    return delimitadorIzquierda + cadena + delimitadorDerecha;
+  return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos = crearCadena.bind();
-let textoGuiones = crearCadena.bind();
-let textoUnderscore = crearCadena.bind();
+let textoAsteriscos = crearCadena.bind(this, "*", "*");
+let textoGuiones = crearCadena.bind(this, "-", "-");
+let textoUnderscore = crearCadena.bind(this, "_", "_");
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   counter,
-   cacheFunction,
-   getNombreInstructor,
-   getNombreAlumno,
-   textoAsteriscos,
-   textoGuiones,
-   textoUnderscore,
+  counter,
+  cacheFunction,
+  getNombreInstructor,
+  getNombreAlumno,
+  textoAsteriscos,
+  textoGuiones,
+  textoUnderscore,
 };
