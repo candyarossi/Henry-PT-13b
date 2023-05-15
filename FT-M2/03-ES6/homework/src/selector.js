f3932bb361a16d4b1e4f12e127af7c590336238f
Candela Yarossi <candyarossi@gmail.com>
2023-05-15 16:48:58 -0300

    HW M2 03

--- FT-M2/03-ES6/homework/src/selector.js
+++ FT-M2/03-ES6/homework/src/selector.js
@@ -9,6 +9,13 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if (matchFunc(startEl)) resultSet.push(startEl);
+  for (let i = 0; i < startEl.children.length; i++) {
+    let child = startEl.children[i];
+    let elementFound = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...elementFound];
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +23,10 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if (selector[0] === "#") return "id";
+  if (selector[0] === ".") return "class";
+  if (selector.split(".").length === 2) return "tag.class";
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,9 +38,29 @@ var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = function (element) {
+      return "#" + element.id === selector;
+    };
   } else if (selectorType === "class") {
+    matchFunction = function (element) {
+      let classes = element.classList;
+      for (let i = 0; i < classes.length; i++) {
+        if ("." + classes[i] === selector) return true;
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
+    matchFunction = function (element) {
+      let [tagName, className] = selector.split(".");
+      return (
+        matchFunctionMaker(tagName)(element) &&
+        matchFunctionMaker("." + className)(element)
+      );
+    };
   } else if (selectorType === "tag") {
+    matchFunction = function (element) {
+      return element.localName === selector;
+    };
   }
   return matchFunction;
 };
